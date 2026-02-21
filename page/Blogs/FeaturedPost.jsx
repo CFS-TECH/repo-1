0a1568244d2c03d42874e8b963d69167683c7d35
vijyapandey <vijyapandey22@gmail.com>
2026-02-21 17:44:44 +0530

    page Setup 01

--- page/Blogs/FeaturedPost.jsx
+++ page/Blogs/FeaturedPost.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const FeaturedPost = () => {
   return (
-    <div>FeaturedPost</div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio atque voluptates magni. Recusandae repellendus suscipit quam beatae rerum veniam similique maxime commodi fugit voluptatum, iure impedit nulla dolores, illo ipsum.stat
+    </div>
   )
 }
 
