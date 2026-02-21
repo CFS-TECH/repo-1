fac89443594c07da81ed96a2f3bf83bc6ebb3980
vijyapandey <vijyapandey22@gmail.com>
2026-02-21 17:48:40 +0530

    page Setup 02

--- page/Blogs/FeaturedPost.jsx
+++ page/Blogs/FeaturedPost.jsx
@@ -3,7 +3,29 @@ import React from 'react'
 const FeaturedPost = () => {
   return (
     <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio atque voluptates magni. Recusandae repellendus suscipit quam beatae rerum veniam similique maxime commodi fugit voluptatum, iure impedit nulla dolores, illo ipsum.stat
+       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore eligendi, ab maiores quos in facilis tempore voluptatum blanditiis illum tempora laborum praesentium iusto nostrum neque possimus, pariatur amet temporibus aperiam.
+       Qui autem officia quis iste consectetur saepe ullam quasi. Mollitia maxime delectus nulla eaque, quibusdam rerum dolores iste aliquam ad quam nemo eos? Voluptatibus odio eos facilis ratione dicta ex!
+       Pariatur, facere laboriosam! A excepturi, numquam, vero animi laboriosam optio veritatis quasi corporis, quod id eum libero commodi dolores tempore minus molestias voluptatem odit explicabo! Iste et ab delectus aspernatur!
+       Aut quas quae officia neque harum nisi exercitationem minima corporis temporibus sint molestias, id eos quam! Labore, voluptates! Velit placeat perferendis quas a facilis ea molestias tenetur suscipit voluptatibus fuga?
+       Vitae eveniet qui asperiores unde saepe eos a repellat excepturi at molestiae provident minima soluta sint dolorum maxime nemo ea aperiam distinctio quibusdam, facilis optio ratione tempora corrupti odio. Vitae.
+       Asperiores esse delectus corporis minus, reiciendis, ut explicabo fugiat illo nihil earum eveniet temporibus! Id, aliquid! Quibusdam tempora accusamus sequi, dignissimos corporis cupiditate praesentium harum iusto eius voluptatem, tenetur eos.
+       Cumque, est assumenda sint impedit nemo totam quibusdam? Nobis, qui adipisci velit nam illum officiis ut dolore, eveniet voluptatum, dolorum placeat amet quod eum! Minima inventore ullam culpa quasi maxime.
+       Ab provident iste quaerat ut aut mollitia nulla architecto aspernatur doloribus voluptas. Accusamus quas architecto ducimus cum, quia nesciunt id est aperiam adipisci mollitia numquam tenetur nobis provident quos nam.
+       Optio, sequi quo error quisquam dolor similique harum nemo quasi rerum reprehenderit in ipsa praesentium corporis, distinctio ad eum blanditiis quod porro officia ratione sunt officiis rem iusto? Sint, natus.
+       Exercitationem, corrupti explicabo inventore laboriosam ea impedit molestiae fugit, totam, eius consequatur sit obcaecati culpa ut magnam unde dignissimos praesentium cupiditate. Quidem accusamus iste dolorem aliquid reprehenderit! Et, placeat dolores!
+       Cum officiis nobis magnam amet. Mollitia repellat labore nesciunt aspernatur dolor inventore, magni fuga iste temporibus, provident, sapiente nulla quam minima culpa ipsum velit possimus animi omnis cum sed ea.
+       Praesentium sit voluptatem ratione doloremque recusandae iure optio dolor autem officiis hic libero iusto, quas, quidem possimus eum qui, veniam consectetur nisi dolores iste! Ut voluptas consequuntur quidem dolor optio!
+       Nulla consequatur illo impedit? Earum labore voluptatem numquam unde itaque quo, nihil, accusamus cum molestiae, blanditiis ipsam aperiam ipsa veniam suscipit dolores aliquam debitis? Hic optio explicabo perspiciatis totam recusandae.
+       Voluptatibus, commodi et. Magni obcaecati corporis sint et optio nisi fuga at, amet voluptatum, veniam rem fugit dignissimos distinctio laudantium ducimus excepturi recusandae voluptate itaque! Tenetur quasi esse nulla consectetur?
+       Aliquam maxime, possimus quo rerum doloremque molestias veniam eaque asperiores neque nemo saepe nulla reprehenderit illum voluptatum similique corporis ipsum exercitationem voluptatem excepturi quae? Aut iure nihil odit quidem culpa.
+       Accusantium nam nulla placeat impedit? Quidem iusto illum voluptatibus cumque rem in eum ad excepturi dolorem labore totam enim, consectetur modi officiis? Odit reprehenderit vel quae blanditiis fuga quos aliquid.
+       Earum laborum perspiciatis vero nesciunt aliquam quas nam assumenda expedita recusandae omnis facilis, doloribus veniam error neque consectetur beatae delectus illo ratione non ab dolorem officia eius est. Sed, et?
+       Modi laudantium voluptatem veritatis voluptas, possimus sit commodi provident quasi molestiae magni voluptate quae neque dignissimos quis sint. Architecto perferendis pariatur quod! Quam asperiores, fugit quisquam eveniet reiciendis ad quasi!
+       Perspiciatis quia eveniet pariatur tempora. Temporibus officiis delectus deleniti quis non assumenda accusantium, quibusdam molestiae fugit inventore facere cupiditate! Deleniti totam optio similique culpa, voluptatem quidem provident magnam laborum numquam.
+       Labore ullam, debitis sunt, nobis alias explicabo consectetur vel, eos mollitia molestiae reiciendis distinctio assumenda! Repudiandae mollitia odio quia molestias assumenda! Nesciunt aspernatur consectetur tempora laboriosam dolorum aut? Ab, nisi!
+       Vero nemo debitis repellendus officia cupiditate fugiat unde eius natus pariatur aspernatur tempore numquam harum excepturi corrupti a veniam accusamus, asperiores sed minus laudantium atque labore ut quam? Quis, officia!
+       Error id dolores dolorum ex a possimus! Itaque qui esse architecto incidunt animi. Temporibus illum quod neque eos dolor deserunt molestiae aliquam, perferendis sunt id earum aspernatur, itaque at iure.
+       Illum eaque unde fugit deserunt consectetur placeat vitae! Iusto, provident molestias ab illum iure ullam eveniet voluptate nulla sint, sequi ut adipisci voluptatibus labore! Tempore quae consectetur commodi laborum pariatur!
     </div>
   )
 }
