88f0b91ae3b2e225c9fdbb5f66a0228970d623a1
amar2mail9 <amar2mail9@gmail.com>
2026-03-02 14:56:09 +0530

    SEO Page Complete

---
+++ app/services/seo/page.js
@@ -0,0 +1,12 @@
+import SEOPage from '@/page/Services/ServicesDetails/SEO/SEOPage'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <SEOPage/>
+    </div>
+  )
+}
+
+export default page
